b3382e3a1750e94a6c3c424557210d2fb83e60e6
vika <vika.0x2606@gmail.com>
2026-08-06 19:16:17 +0800

    移除 direnv，改用 mise 的 [env] 管理目录级环境变量
    mise activate 去掉 --shims 以启用 chpwd hook；顺带补 lazygit 到工具列表。

--- zsh/.config/zsh/conf.d/30-tools.zsh
+++ zsh/.config/zsh/conf.d/30-tools.zsh
@@ -1,7 +1,7 @@
 # External tool initialization + fzf defaults + editor.
 
 # Sync: needed before the prompt renders.
-eval "$(mise activate zsh --shims)" 2>/dev/null
+eval "$(mise activate zsh)" 2>/dev/null
 eval "$(starship init zsh)" 2>/dev/null
 
 # Defer when zsh-defer is loaded, otherwise inline-eval as a fallback.
@@ -15,7 +15,6 @@ _lazy() {
 _lazy 'eval "$(pathctl activate)"'
 _lazy 'eval "$(zoxide init zsh)"'
 _lazy '(( $+commands[atuin]  )) && eval "$(atuin init zsh --disable-up-arrow)"'
-_lazy '(( $+commands[direnv] )) && eval "$(direnv hook zsh)"'
 
 # forgit: diff/show pager 自动继承 git core.pager (delta)
 export FORGIT_DIR_VIEW='eza -1 --color=always --icons --group-directories-first'
